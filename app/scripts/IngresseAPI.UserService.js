f6f10e79822d054d2ed2f5a8d2facee4e13bed4f
Junior Silvestre <junior.silvestre@ingresse.com>
2016-10-10 19:13:18 -0300

    add jwt token in user service
    fix crypto dependency in bower.json

--- app/scripts/IngresseAPI.UserService.js
+++ app/scripts/IngresseAPI.UserService.js
@@ -5,29 +5,44 @@ angular.module('ingresseSDK').service('IngresseApiUserService', function UserSer
         data: {},
         credentials: {},
         getCredentials: function () {
-            this.credentials.userId = ipCookie('userId');
-            this.credentials.token = ipCookie('token');
+            this.credentials.userId   = ipCookie('userId');
+            this.credentials.token    = ipCookie('token');
+            this.credentials.jwtToken = ipCookie('jwtToken');
 
-            if (this.credentials.userId || this.credentials.token) {
+            if (this.credentials.userId || this.credentials.token || this.credentials.jwtToken) {
                 return this.credentials;
             }
 
             return false;
         },
-        clearCredentials: function(){
+        clearCredentials: function () {
             ipCookie('userId', '', { expires: -1, domain: '.ingresse.com' });
             ipCookie('token', '', { expires: -1, domain: '.ingresse.com' });
+            ipCookie('jwtToken', '', { expires: -1, domain: '.ingresse.com' });
             ipCookie('userId', '', { expires: -1});
             ipCookie('token', '', { expires: -1});
-            this.data = null;
-            this.credentials.userId = null;
-            this.credentials.token = null;
+            ipCookie('jwtToken', '', { expires: -1});
+
+            this.data                 = null;
+            this.credentials.userId   = null;
+            this.credentials.token    = null;
+            this.credentials.jwtToken = null;
         },
-        saveCredentials: function (token, userId){
+        saveCredentials: function (token, userId, jwtToken) {
             this.credentials.userId = userId;
-            this.credentials.token = token;
+            this.credentials.token  = token;
+            
+            if (jwtToken) {
+                this.credentials.jwtToken = jwtToken;
+                ipCookie('jwtToken', jwtToken, { expires: 7, domain: '.ingresse.com' });
+                
+            } else if (ipCookie('jwtToken')) {
+                this.credentials.jwtToken = ipCookie('jwtToken');
+            }
+
             ipCookie('userId', userId, { expires: 7, domain: '.ingresse.com' });
             ipCookie('token', token, { expires: 7, domain: '.ingresse.com' });
+
             $rootScope.$broadcast('userSessionSaved');
         },
         saveLocation: function(location){
