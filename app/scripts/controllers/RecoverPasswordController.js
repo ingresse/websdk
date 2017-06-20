4db66ae0b106285fbd1d608cac2fecc27f4ac8c3
Jefferson Silva <spalla69@gmail.com>
2017-06-20 16:01:05 -0300

    ADD the recover password in emulator

--- app/scripts/controllers/RecoverPasswordController.js
+++ app/scripts/controllers/RecoverPasswordController.js
@@ -21,10 +21,47 @@ angular.module('ingresseEmulatorApp')
 
       var identifier = $scope.fields.recoverPassword.identifier.model;
 
-      QueryService.setSearchParams('recoverPassword', $scope.fields
-        .recoverPassword.identifier, filters);
+      QueryService.setSearchParams('recoverPassword', $scope.fields.recoverPassword.identifier);
 
       ingresseAPI.recover.recoverPassword(identifier)
+        .then(function (response) {
+          EmulatorService.addResponse(response, true);
+        })
+        .catch(function (error) {
+          EmulatorService.addResponse(error, false);
+        })
+        .finally(function () {
+          $scope.isLoading = false;
+        });
+    };
+
+    $scope.validateHash = function () {
+      $scope.isLoading = true;
+
+      var filters = QueryService.getFiltersByTab($scope.fields.validateHash);
+
+      QueryService.setSearchParams('validateHash', $scope.fields.validateHash.identifier, filters);
+
+      ingresseAPI.recover.validateHash(filters)
+        .then(function (response) {
+          EmulatorService.addResponse(response, true);
+        })
+        .catch(function (error) {
+          EmulatorService.addResponse(error, false);
+        })
+        .finally(function () {
+          $scope.isLoading = false;
+        });
+    };
+
+    $scope.updatePassword = function () {
+      $scope.isLoading = true;
+
+      var filters = QueryService.getFiltersByTab($scope.fields.updatePassword);
+
+      QueryService.setSearchParams('updatePassword', $scope.fields.updatePassword.identifier, filters);
+
+      ingresseAPI.recover.updatePassword(filters)
         .then(function (response) {
           EmulatorService.addResponse(response, true);
         })
@@ -48,6 +85,53 @@ angular.module('ingresseEmulatorApp')
           required: true
         },
         fields: []
+      },
+      validateHash: {
+        label: 'Validate Password Hash',
+        action: $scope.validateHash,
+        fields: [
+          {
+            label: 'email',
+            model: '',
+            type: 'text',
+            disabled: false,
+            required: true
+          },
+          {
+            label: 'hash',
+            model: '',
+            type: 'text',
+            disabled: false,
+            required: true
+          }
+        ]
+      },
+      updatePassword: {
+        label: 'Update Password',
+        action: $scope.updatePassword,
+        fields: [
+          {
+            label: 'email',
+            model: '',
+            type: 'text',
+            disabled: false,
+            required: true
+          },
+          {
+            label: 'hash',
+            model: '',
+            type: 'text',
+            disabled: false,
+            required: true
+          },
+          {
+            label: 'password',
+            model: '',
+            type: 'text',
+            disabled: false,
+            required: true
+          }
+        ]
       }
     };
   });
