d3c33364d57046fe6fcf858e7773b7296fc8972e
Luís Lança <luis_lanca@outlook.com>
2023-09-06 15:05:35 -0300

    feat: adding discount coupon error (#141)

--- app/scripts/constants/ErrorsConstant.js
+++ app/scripts/constants/ErrorsConstant.js
@@ -7,6 +7,10 @@ angular.module('ingresseSDK')
     message: 'A senha atual não está correta.',
   },
   {
+    codes: [103],
+    message: 'Parece que você já realizou compras com esse cupom de desconto e atingiu seu limite.Remova o cupom utilizado para prosseguir com a compra.'
+  },
+  {
     codes: [1001],
     message: 'Desculpe, mas você precisa selecionar pelo menos um ingresso.',
   },
